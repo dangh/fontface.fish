30f7432233854554bf1262d365d945ad74486330
Dang <huynhminhdang@gmail.com>
2020-12-11 18:12:52 +0800

    Create fontface.fish

---
+++ fontface.fish
@@ -0,0 +1,9 @@
+function fontface --argument-names style text --description "convert text to monospace font"
+  set --local origin ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789
+  set --local math_monospace 𝙰𝙱𝙲𝙳𝙴𝙵𝙶𝙷𝙸𝙹𝙺𝙻𝙼𝙽𝙾𝙿𝚀𝚁𝚂𝚃𝚄𝚅𝚆𝚇𝚈𝚉𝚊𝚋𝚌𝚍𝚎𝚏𝚐𝚑𝚒𝚓𝚔𝚕𝚖𝚗𝚘𝚙𝚚𝚛𝚜𝚝𝚞𝚟𝚠𝚡𝚢𝚣𝟶𝟷𝟸𝟹𝟺𝟻𝟼𝟽𝟾𝟿
+  set --local replacement $style
+  for i in (seq (string length $origin))
+    set text (string replace --all (string sub --start $i --length 1 $origin) (string sub --start $i --length 1 $replacement) $text)
+  end
+  echo $text
+end
