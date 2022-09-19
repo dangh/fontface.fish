9ecbb4e86085b7fc5a50535d144e82e2147626f3
Dang <huynhminhdang@gmail.com>
2022-09-19 10:08:23 +0700

    use short flags

--- functions/fontface.fish
+++ functions/fontface.fish
@@ -1,8 +1,8 @@
-function fontface --argument-names style text --description "convert text to monospace font"
-  set --local origin ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789
-  set --local math_monospace 𝙰𝙱𝙲𝙳𝙴𝙵𝙶𝙷𝙸𝙹𝙺𝙻𝙼𝙽𝙾𝙿𝚀𝚁𝚂𝚃𝚄𝚅𝚆𝚇𝚈𝚉𝚊𝚋𝚌𝚍𝚎𝚏𝚐𝚑𝚒𝚓𝚔𝚕𝚖𝚗𝚘𝚙𝚚𝚛𝚜𝚝𝚞𝚟𝚠𝚡𝚢𝚣𝟶𝟷𝟸𝟹𝟺𝟻𝟼𝟽𝟾𝟿
+function fontface -a style text -d "convert text to monospace font"
+  set -l origin ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789
+  set -l math_monospace 𝙰𝙱𝙲𝙳𝙴𝙵𝙶𝙷𝙸𝙹𝙺𝙻𝙼𝙽𝙾𝙿𝚀𝚁𝚂𝚃𝚄𝚅𝚆𝚇𝚈𝚉𝚊𝚋𝚌𝚍𝚎𝚏𝚐𝚑𝚒𝚓𝚔𝚕𝚖𝚗𝚘𝚙𝚚𝚛𝚜𝚝𝚞𝚟𝚠𝚡𝚢𝚣𝟶𝟷𝟸𝟹𝟺𝟻𝟼𝟽𝟾𝟿
   for i in (seq (string length $origin))
-    set text (string replace --all (string sub --start $i --length 1 $origin) (string sub --start $i --length 1 $$style) $text)
+    set text (string replace -a (string sub -s $i -l 1 $origin) (string sub -s $i -l 1 $$style) $text)
   end
   echo $text
 end
